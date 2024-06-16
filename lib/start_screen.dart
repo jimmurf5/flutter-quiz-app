448b162e00fdefcc590eea0b3658cbfb92b82d15
James Murphy <jmurphy98@qub.ac.uk>
2024-06-16 13:16:27 +0100

    add image and text

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,14 +4,24 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
-  Widget build(context){
-    return const Center(child: Text('start screen'),
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white
+              ),
+          )
+        ],
+      ),
     );
   }
 }
-
-
-
-
-
-
